97b75e9b71f7ad214ae992f885ccb6674606f23f
David-Albarracin <90213684+David-Albarracin@users.noreply.github.com>
2023-12-19 14:00:05 -0500

    Change all Global Core

--- confgitinit.bat
+++ confgitinit.bat
@@ -3,6 +3,6 @@ SETLOCAL EnableDelayedExpansion
 set condition=true
 git config --global user.name "David-Albarracin" 
 git config --global user.email "90213684+David-Albarracin@users.noreply.github.com"
-git config --global core.editor “code --wait”
+git config --global --replace-all core.editor "code --wait"
 git config --global init.defaultBranch main
 ENDLOCAL
