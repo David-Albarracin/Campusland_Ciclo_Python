cd55713f8cd04a506a5ef97b7ee940acc5503bf3
David-Albarracin <90213684+David-Albarracin@users.noreply.github.com>
2024-01-03 09:14:23 -0500

    .

--- Inventario Archivos/main.py
+++ Inventario Archivos/main.py
@@ -1,9 +1,16 @@
+if __name__ == "__main__":
+    pass
+
+
+
 import json
 
 data = {
     "id": "001",
     "nombre": "pepe"
 }
+
+data.pop()
 
 with open("./Archivos/db.json", "w") as db:
     json.dump({data["id"]:data}, db, indent=2)
